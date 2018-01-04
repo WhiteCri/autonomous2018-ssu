d2b5127ec7eba981a620980fa2d70c83f73d5850
WhiteCri <sjrnfu12@naver.com>
2018-01-04 22:33:08 +0900

    시리얼 통신 모듈 작성

---
+++ sensor_configurator/include/SerialCommunicator.h
@@ -0,0 +1,35 @@
+#ifndef _SERIALCOMMUNICATOR_H
+#define _SERIALCOMMUNICATOR_H
+#endif
+
+#include <serial/serial.h>
+#include <iostream>
+
+using std::string;
+#define DD 3
+class SerialCommunicator{
+public:
+  SerialCommunicator(const string& port, int baudrate = 9600, int timeout = 1000);
+  bool isValid();
+  const std::string& read();
+  const std::string& getRaw();
+  void write(const string& write);
+  const string& getErrorMsg();
+private:
+  //serial communication setting
+  serial::Serial ser;
+  string port_;
+  int baudrate_;
+  int timeout_;
+
+  //hold raw data
+  string raw;
+  //hold oneline from raw
+  string now;
+
+  //hold errorMsg
+  static string errorMsg;
+
+  //check if valid instance
+  bool validInstance;
+};
---
+++ sensor_configurator/src/SerialCommunicator.cpp
@@ -0,0 +1,68 @@
+#include "SerialCommunicator.h"
+#include <string>
+#include <sstream>
+string SerialCommunicator::errorMsg = "";
+SerialCommunicator::SerialCommunicator(const std::string& port, int baudrate, int timeout)
+    //serial setting member
+    : port_(port), baudrate_(baudrate), timeout_(timeout), validInstance(false),
+    //data member
+    raw(""), now("")
+{
+  try{
+    ser.setPort(port_.c_str());
+    ser.setBaudrate(baudrate_);
+    serial::Timeout to = serial::Timeout::simpleTimeout(timeout_);
+    ser.setTimeout(to);
+    ser.open();
+    //0 is error num, but i don't know each number point which err. i just put it to
+    //complete function call
+    if(!ser.isOpen()) throw serial::IOException("ser.isOpen() error!",__LINE__,0);
+  }
+  catch (serial::IOException& e)
+  {
+      //hold error Msg
+      errorMsg = e.what();
+      validInstance = false;
+  }
+  catch (std::exception& e)
+  {
+    errorMsg = string("Not a serial IOException : ") + e.what();
+  }
+
+  validInstance = true;
+}
+
+bool SerialCommunicator::isValid(){
+  return validInstance;
+}
+
+const string& SerialCommunicator::read(){
+  if(!isValid()) {
+    now = "";
+    raw = "";
+    return now;
+  }
+
+  if(ser.available()){
+      raw = ser.read(ser.available());
+
+      //one line parsing
+      std::stringstream ss(raw);
+      std::getline(ss,now);
+      std::getline(ss,now);
+  }
+  else raw = now = "";
+  return now;
+}
+
+const string& SerialCommunicator::getRaw(){
+  return raw;
+}
+
+void SerialCommunicator::write(const string& data){
+  ser.write(data.c_str());
+}
+
+const string& SerialCommunicator::getErrorMsg(){
+  return SerialCommunicator::errorMsg;
+}
