7bb378214b868e73e195bbf514a8d72f741037d6
Hwi Jin <huisam@naver.com>
2018-01-26 15:46:45 +0900

    GPS_Ver.1

---
+++ nmea_navsat_driver/include/GPSParameter.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define GPS_Sentence_ID 1
+#define GPS_Status 3
+#define GPS_Latitude 4
+#define GPS_NS_Indicator 5
+#define GPS_Longitude 6
+#define GPS_EW_Indicator 7
+#define GPS_Speed_Over_Ground 8
+#define GPS_Course_Over_Ground 9
+#define GPS_Publish 10
