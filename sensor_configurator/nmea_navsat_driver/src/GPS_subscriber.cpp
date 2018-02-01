b7136a4c24cf2c2842839526463e0231e405d7c6
Hwi Jin <huisam@naver.com>
2018-02-01 13:52:30 +0900

    GPS Ver2.: transformer 추가 & launch 파일 작성

--- sensor_configurator/nmea_navsat_driver/src/GPS_subscriber.cpp
+++ sensor_configurator/nmea_navsat_driver/src/GPS_subscriber.cpp
@@ -1,12 +1,10 @@
 //Header for Ros
 #include"ros/ros.h"
 #include"nmea_navsat_driver/nmea_msg.h"
-#include"nmea_navsat_driver/NmeaMsg.h"
-#include"GPSParameter.h"
+#include"nmea_navsat_driver/MyMsg.h"
 //Use String 함수
 #include<string>
 #include<sstream>
-
 
 
 //Subscribe 후 Publish하기 위한 callback 함수
@@ -15,55 +13,47 @@ void msgCallback(const nmea_navsat_driver::nmea_msg::ConstPtr& msg2){
     std::string raw = msg2->data;
     // Publish하기 위한 변수선언
     ros::NodeHandle nh;
-    ros::Publisher GPS_publisher = nh.advertise<nmea_navsat_driver::NmeaMsg>("raw/GNSS",100);
-    nmea_navsat_driver::NmeaMsg msg;
+    ros::Publisher GPS_publisher = nh.advertise<nmea_navsat_driver::MyMsg>("raw/GPS",100);
+    nmea_navsat_driver::MyMsg msg;
     // Data Pharse 변수선언
     std::stringstream ss(raw);
     std::string data;
     // 속도 변환하기 위한 변수선언
     double speed;
-    for(int i=1; i<11;i++){
-        std::getline(ss,data,',');
-        //ROS_INFO("%s",data.c_str());
-        switch(i){
-            case GPS_Sentence_ID:
-                if(data.compare("$GPRMC")){ // GPRMC Data Format이 아니면 종료
-                //  ROS_INFO("WRONG DATA FORMAT");
-                    i=11;
-                }
-                break;
-            case GPS_Status:
-                msg.Status = data;
-                break;
-            case GPS_Latitude:
-                msg.Latitude = data;
-                break;
-            case GPS_NS_Indicator:
-                msg.NSIndicator = data;
-                break;
-            case GPS_Longitude:
-                msg.Longitude = data;
-                break;
-            case GPS_EW_Indicator:
-                msg.EWIndicator = data;
-                break;
-            case GPS_Speed_Over_Ground:
-                speed = atof(data.c_str());
-                speed *= 1.852;
-                data = std::to_string(speed);
-                msg.Speed = data;
-                break;
-            case GPS_Course_Over_Ground:
-                msg.Course = data;
-                break;
-            case GPS_Publish:
-                GPS_publisher.publish(msg);
-                break;
-            default:
-                break;
-            }
-       
-    }
+
+    std::getline(ss,data,','); // first
+        // testing Code : ROS_INFO("%s",data.c_str());
+    if(data.compare("$GPRMC")){ // GPRMC Data Format이 아니면 종료
+    //  ROS_INFO("WRONG DATA FORMAT");
+        return;
+        }
+    std::getline(ss,data,','); // second
+    
+    std::getline(ss,data,','); // third
+    msg.Status = data;
+    
+    std::getline(ss,data,','); // fourth
+    msg.Latitude = data;
+    
+    std::getline(ss,data,','); // fivth
+    msg.NSIndicator = data;
+    
+    std::getline(ss,data,','); // sixth
+    msg.Longitude = data;
+    
+    std::getline(ss,data,','); // seventh
+    msg.EWIndicator = data;
+    
+    std::getline(ss,data,','); // eigth
+    speed = atof(data.c_str());
+    speed *= 1.852;
+    data = std::to_string(speed);
+    msg.Speed = data;
+    
+    std::getline(ss,data,','); // ninth
+    msg.Course = data;
+    
+    GPS_publisher.publish(msg); // parse            
     return;
 }
 
@@ -71,8 +61,8 @@ int main(int argc, char *argv[]){
     ros::init(argc, argv, "GPS_subscriber");
     ros::NodeHandle nh;
     ros::Subscriber nmea_subscriber = nh.subscribe("Nmea_Publish",100,msgCallback);
-    ros::Publisher GPS_publisher = nh.advertise<nmea_navsat_driver::NmeaMsg>("raw/GNSS",100);
-  
+    ros::Publisher GPS_publisher = nh.advertise<nmea_navsat_driver::MyMsg>("raw/GPS",100);
+
     ros::spin();
          
     return 0;
