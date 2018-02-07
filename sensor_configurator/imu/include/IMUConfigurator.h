2ae7ff104757c37462a1f7e8a18c07228e5bab5c
lkm2835 <lkm2835@gmail.com>
2018-02-07 23:07:17 +0900

    sensor_msgs 형태 완성, covariance 에 0, -1 적절히 넣어줘야함, Roll Pitch Yaw 값을 이용해 x,y,z,w값 넣어줘야함

--- sensor_configurator/imu/include/IMUConfigurator.h
+++ sensor_configurator/imu/include/IMUConfigurator.h
@@ -1,15 +1,17 @@
 #include <serial/serial.h>
-#include "imu/IMU_data.h"
+#include "sensor_msgs/Imu.h"
 
 #define IMUFRE 5
+    
+static uint32_t m = 0;
 
 class IMUConfigurator
 {
 public:
     std::string parse();
     int serialCommucation(char* path_);
-    void RPY(std::string parse);
-    imu::IMU_data msg;
+    void RPY(std::string parse,ros::NodeHandle nh);
+    sensor_msgs::Imu imu_msg;
 
 private:
     serial::Serial ser;
--- sensor_configurator/imu/src/IMUConfigurator.cpp
+++ sensor_configurator/imu/src/IMUConfigurator.cpp
@@ -37,8 +37,10 @@ int IMUConfigurator::serialCommucation(char* path_)
     }
 }
 
-void IMUConfigurator::RPY(std::string parse)
+void IMUConfigurator::RPY(std::string parse,ros::NodeHandle nh)
 {
+        ros::Publisher imu_data_pub = nh.advertise<sensor_msgs::Imu>("raw/imu", 100);
+
         ROS_INFO("%s",parse.c_str());
 
         std::stringstream cc(parse);
@@ -54,14 +56,70 @@ void IMUConfigurator::RPY(std::string parse)
         std::getline(cc,P,',');
         std::getline(cc,Y,',');
         
-        msg.Roll = std::stod(R);
-        msg.Pitch = std::stod(P);
-        msg.Yaw = std::stod(Y);
+        double Roll = std::stod(R);
+        double Pitch = std::stod(P);
+        double Yaw = std::stod(Y);
+
+    //Roll Pitch Yaw 값을 이용해 연산하여 x,y,z,w 값에 넣어줘야함
+    //covariance 에 0 , -1 값을 적절히 넣어줘야함 
+
+    imu_msg.header.seq = m%0xffffffff;
+    imu_msg.header.stamp = ros::Time::now();
+    imu_msg.header.frame_id = "imu";
+
+    imu_msg.orientation.x = 0;
+    imu_msg.orientation.y = 0;
+    imu_msg.orientation.z = 0;
+    imu_msg.orientation.w = 0;
+    imu_msg.orientation_covariance[0] = 0;
+    imu_msg.orientation_covariance[1] = 0;
+    imu_msg.orientation_covariance[2] = 0;
+    imu_msg.orientation_covariance[3] = 0;
+    imu_msg.orientation_covariance[4] = 0;
+    imu_msg.orientation_covariance[5] = 0;
+    imu_msg.orientation_covariance[6] = 0;
+    imu_msg.orientation_covariance[7] = 0;
+    imu_msg.orientation_covariance[8] = 0;
+
+    imu_msg.angular_velocity.x = 0;
+    imu_msg.angular_velocity.y = 0;
+    imu_msg.angular_velocity.z = 0;
+    imu_msg.angular_velocity_covariance[0] = 0;
+    imu_msg.angular_velocity_covariance[1] = 0;
+    imu_msg.angular_velocity_covariance[2] = 0;
+    imu_msg.angular_velocity_covariance[3] = 0;
+    imu_msg.angular_velocity_covariance[4] = 0;
+    imu_msg.angular_velocity_covariance[5] = 0;
+    imu_msg.angular_velocity_covariance[6] = 0;
+    imu_msg.angular_velocity_covariance[7] = 0;
+    imu_msg.angular_velocity_covariance[8] = 0;
+
+    imu_msg.linear_acceleration.x = 0;
+    imu_msg.linear_acceleration.y = 0;
+    imu_msg.linear_acceleration.z = 0;
+    imu_msg.linear_acceleration_covariance[0] = 0;
+    imu_msg.linear_acceleration_covariance[1] = 0;
+    imu_msg.linear_acceleration_covariance[2] = 0;
+    imu_msg.linear_acceleration_covariance[3] = 0;
+    imu_msg.linear_acceleration_covariance[4] = 0;
+    imu_msg.linear_acceleration_covariance[5] = 0;
+    imu_msg.linear_acceleration_covariance[6] = 0;
+    imu_msg.linear_acceleration_covariance[7] = 0;
+    imu_msg.linear_acceleration_covariance[8] = 0;
+
+    ++m;
+    
+
+////
+
                 
-        ROS_INFO("%.2f",msg.Roll);
-        ROS_INFO("%.2f",msg.Pitch);
-        ROS_INFO("%.2f",msg.Yaw);
+        ROS_INFO("%.2f",Roll);
+        ROS_INFO("%.2f",Pitch);
+        ROS_INFO("%.2f",Yaw);
 
         ROS_INFO("---------");
+
+
+        imu_data_pub.publish(imu_msg);
 
 }
