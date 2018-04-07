adc42590aa572a94968cb067c38c7d8405397aa1
huisam <huisam@naver.com>
2018-04-08 00:48:13 +0900

    Dynamic 임시로 1.5m 만 detect하게 설정 / 더 좋은 flag 모색중

--- sensor_configurator/improve_lidar/src/improve_lidar.cpp
+++ sensor_configurator/improve_lidar/src/improve_lidar.cpp
@@ -8,26 +8,30 @@
 #define Laser_Filter_end 300
 #define check_dynamic 5
 #define check_UTurn 2
-#define dynamic_distance_point 0.5
+#define dynamic_distance_point 0.2
 #define UTurn_distance_point 0.1
-#define frequency 1
+#define frequency 0.8
 
-#define test_uturn
+#define test_dynamic
 
 static double inf = std::numeric_limits<double>::infinity();
 static bool dynamic_obstacle, u_turn;
+static int before_detect = 0;
 
 bool checkdynamic(std::vector<float> ranges, int number)
 {
-   // obstacle_detector::Obstacles test;
-    //ROS_INFO("%lf",test.segments.data->first_point.x);
-
+    int max_range = check_dynamic + 1;
+    /* detect size 크기 조절 */
+    if( (ranges[number+max_range] == inf || ranges[number+max_range] == -inf) &&
+        (ranges[number-max_range] == inf || ranges[number-max_range] == -inf) )
+        {}
+    else
+        return false;
 
     int offset;
     for(offset=1; offset<=check_dynamic; offset++)
     {
-        if( (ranges[number] > 100 ) && (ranges[number] < 5) &&
-          (fabs(ranges[number]-ranges[number+offset])<dynamic_distance_point) &&
+        if((fabs(ranges[number]-ranges[number+offset])<dynamic_distance_point) &&
           (fabs(ranges[number]-ranges[number-offset])<dynamic_distance_point) )
             continue;
         else
@@ -41,23 +45,29 @@ bool checkUTurn(std::vector<float>ranges, int number)
 {
     int offset;
     int max_range = check_UTurn + 1;
-
+    /* detect size 크기 조절 */
     if( (ranges[number+max_range] == inf || ranges[number+max_range] == -inf) &&
         (ranges[number-max_range] == inf || ranges[number-max_range] == -inf) )
         {}
     else
         return false;
-    
-
+    /* detection */
     for(offset=1; offset<=check_UTurn; offset++)
     {
         if( (ranges[number+offset] != inf) && (ranges[number-offset] != inf) &&
-             (fabs(ranges[number]-ranges[number+offset]) < UTurn_distance_point) &&
-           (fabs(ranges[number]-ranges[number-offset]) < UTurn_distance_point) )
-            continue;
+            (fabs(ranges[number]-ranges[number+offset]) < UTurn_distance_point) &&
+           (fabs(ranges[number]-ranges[number-offset]) < UTurn_distance_point))
+                continue;
+            
         else
             return false;
-    } 
+    }
+    /* 이전 상태와 range값 비교해서 최대한 붙어 있게 설정 */
+    if(before_detect == 0)
+        before_detect = number;
+    if((fabs(ranges[number]-ranges[before_detect]) > 0.2))
+        return false;
+    before_detect = number;
     return true;
 }
 
@@ -122,7 +132,7 @@ int main(int argc, char *argv[])
     while(ros::ok())
     {
         ros::spinOnce();
-        //loop_rate.sleep();
+        loop_rate.sleep();
     }
     ros::spin();
 
