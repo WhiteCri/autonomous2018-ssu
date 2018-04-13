e205829a5556e2f01c8b61746cb39781c6f68672
WhiteCri <sjrnfu12@naver.com>
2018-04-14 00:34:10 +0900

    platform_rx 버그수정, launch파일추가

--- sensor_configurator/platform_rx_thread_test/src/platform_rx_thread_test.cpp
+++ sensor_configurator/platform_rx_thread_test/src/platform_rx_thread_test.cpp
@@ -3,14 +3,29 @@
 #include <mutex>
 #include <thread>
 #include <deque>
+#include <exception>
 #include "platform_rx_msg/platform_rx_msg.h"
 
-static constexpr int serial_read_loop_rate = 20;
-static constexpr int pub_loop_rate = 10;
-static constexpr int encoder_memory = 4;
-static constexpr int encoder_gap_memory = encoder_memory - 1;
 serial::Serial ser;
 std::mutex lock;
+
+class ParamReader{
+public:
+    ParamReader(ros::NodeHandle& nh){
+        if(!nh.getParam("rx_thread_test/path", path)) throw std::runtime_error("give me path!");
+        if(!nh.getParam("rx_thread_test/serial_frequency", serial_frequency)) 
+            throw std::runtime_error("give me serial_frequency!");
+        if(!nh.getParam("rx_thread_test/publish_frequency", publish_frequency)) 
+            throw std::runtime_error("give me publish_frequency!");
+        if(!nh.getParam("rx_thread_test/moving_average_element_number", moving_average_element_number))
+            throw std::runtime_error("give me moving_average_element_number!");
+        if(moving_average_element_number <= 1) std::runtime_error("too small element number...");
+    }
+    std::string path;
+    int serial_frequency;
+    int publish_frequency;
+    int moving_average_element_number;
+};
 
 uint8_t packet[18] = {0};
 
@@ -20,26 +35,24 @@ T getParsingData(const uint8_t *dataArray, int startIndex){
     return re_;
 }
 
-void readSerial(){
+void readSerial(int serial_read_loop_rate){
     ros::Rate loop_rate(serial_read_loop_rate);
 
     std::string raw;
-    size_t seq = static_cast<size_t>(-1);
     size_t error_cnt = 0;
     while(true){
-        seq++;
+        //read from serial
         if(ser.available() >= 18) raw = ser.read(ser.available());
         else loop_rate.sleep(); 
 
+        //save in process variable
         const char *check = raw.c_str();
         if((check[0] == 0x53) & (check[1] == 0x54) & (check[2] == 0x58) & check[16] == 0x0D & check[17] == 0x0a ){
             lock.lock();
             for(int i = 0 ; i < 18; ++i) *(packet + i) = *(check + i);        
             lock.unlock();
         }
-        else{
-            loop_rate.sleep();
-        }
+        loop_rate.sleep();
     }
 }
 
@@ -48,11 +61,13 @@ int main (int argc, char** argv){
     ros::init(argc, argv, "rx_thread_test");
     ros::NodeHandle nh;
 
+    ParamReader reader(nh);
+
     ros::Publisher pub = nh.advertise<platform_rx_msg::platform_rx_msg>("raw/platform_rx", 1000);
 
     try
     {
-        ser.setPort("/dev/ttyUSB0");
+        ser.setPort(reader.path);
         ser.setBaudrate(115200);
         serial::Timeout to = serial::Timeout::simpleTimeout(1000);
         ser.setTimeout(to);
@@ -70,19 +85,26 @@ int main (int argc, char** argv){
         return -1;
     }
 
-    
-    std::thread thr(readSerial);
+    std::thread thr(readSerial, reader.serial_frequency);
     thr.detach();
 
     platform_rx_msg::platform_rx_msg msg;
-    ros::Rate loop_rate(pub_loop_rate);
+    ros::Rate loop_rate(reader.publish_frequency);
     size_t seq = 0;
     uint8_t packet_main[18] ={0};
 
     //speed calcuration member
-    std::deque<int32_t> encoder(encoder_memory);
-    std::deque<int32_t> encoderGap(encoder_gap_memory);
+    std::deque<std::pair<int32_t, ros::Time> > encoder(reader.moving_average_element_number);
 
+ //speed = (encoder[0].first - encoder[1].first) / encoderValuePerCycle * distanceValuePerCycle 
+         //   / timeInterval / interval;
+    auto calc_speed =[&]()->double{
+        double total_encoder_gap = encoder.front().first - encoder.back().first;
+        double time_interval = 
+            encoder.front().second.toSec() - encoder.back().second.toSec();
+        double speed = total_encoder_gap / 99.2 * 1.655 / time_interval;
+        return speed;
+    };
     //init
     while(ros::ok()){
         //get packet
@@ -90,17 +112,13 @@ int main (int argc, char** argv){
         for(int i = 0 ; i < 18; ++i) *(packet_main + i) = *(packet + i);
         lock.unlock();
 
-        //calc speed
-        encoder.push_front(getParsingData<int32_t>(packet_main, 11));
+        encoder.push_front(std::make_pair(
+            getParsingData<int32_t>(packet_main, 11),
+            ros::Time::now()
+        ));
         encoder.pop_back();
-        encoderGap.push_front(encoder[0] - encoder[1]);
-        encoderGap.pop_back();
-ROS_INFO("gap[%ld] : %d, encoder : %d",seq, encoderGap[0],encoder[0]);
-        msg.speed = (encoderGap[0] + encoderGap[1] + encoderGap[2]) / 99.2 * 1.655 
-            / 0.1 / 3;
-         //speed = (encoder[0].first - encoder[1].first) / encoderValuePerCycle * distanceValuePerCycle 
-         //   / timeInterval / interval;
 
+        msg.speed = calc_speed();
         msg.steer = getParsingData<int16_t>(packet_main, 8);
         msg.brake = getParsingData<uint8_t>(packet_main, 10);
         msg.seq = seq++;
@@ -108,7 +126,8 @@ ROS_INFO("gap[%ld] : %d, encoder : %d",seq, encoderGap[0],encoder[0]);
         bool estop = getParsingData<uint8_t>(packet_main, 4);
         nh.setParam("estop", estop);
 
-        if(seq > 5)
+        //dirty code...I want to erase...
+        if(seq > (reader.moving_average_element_number + 5))
             pub.publish(msg);     
         loop_rate.sleep();
     }
