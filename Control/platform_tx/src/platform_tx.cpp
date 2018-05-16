7d5eb2618c2f72875d3a15c22911c8085246d2f7
WhiteCri <sjrnfu12@naver.com>
2018-05-16 20:03:01 +0900

    카메랑 gmapping, amcl 계수 설정

--- Control/platform_tx/src/platform_tx.cpp
+++ Control/platform_tx/src/platform_tx.cpp
@@ -86,6 +86,7 @@ void serialWrite(){
         // ALIVE
         packet[11] = static_cast<uint8_t>(alive);    alive = (alive + 1) % 256;
         
+ROS_INFO("alive : %d", packet[11]);
         ser->write(packet,TX_PACKET_LENGTH);
         loop_rate.sleep();
     }
